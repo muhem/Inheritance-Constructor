c6758eaa874c298b8dbe0ac27e9736d77e8fc647
muhem <erwinsyahrul9@gmail.com>
2020-04-10 21:21:57 +0700

    in

---
+++ Tugas_2687/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("Chika", 19);
+            person.GetNameAndAge();
+
+            Teacher teacher = new Teacher("Allan", 40, 190302, "logical information", "Dosen@teacher.amikom.ac.id");
+            teacher.GetNameAndAge();
+
+            Student student = new Student("Muhammad Erwin Syahrul Alim", 19, 2687, "muhammad.2676@student.amikom.ac.id");
+            student.GetNameAndAge();
+        }
+    }
+}
